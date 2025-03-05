dd9137f30effd0ac1f7dfcc072375c0adcbd43fa
sujin <jien911@gmail.com>
2025-03-05 10:55:42 +0900

    first commit

---
+++ src/main/java/com/koyoi/main/KoyoiApplication.java
@@ -0,0 +1,13 @@
+package com.koyoi.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoyoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoyoiApplication.class, args);
+    }
+
+}
