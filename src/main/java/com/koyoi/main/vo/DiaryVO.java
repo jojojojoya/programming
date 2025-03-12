f06ca52edafc7373ad683de823911599ebfe8ab8
nuri <dkssnfl05@naver.com>
2025-03-12 13:02:14 +0900

    Feat: Add DiaryVO and EmotionVO

---
+++ src/main/java/com/koyoi/main/vo/DiaryVO.java
@@ -0,0 +1,22 @@
+package com.koyoi.main.vo;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Data
+public class DiaryVO {
+    private int diary_id;
+    private String diary_title;
+    private String diary_content;
+    private LocalDateTime created_at;
+
+
+    public String getFormattedCreatedAt() {
+        if (created_at != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            return created_at.format(formatter);
+        }
+        return null;
+    }
+}
