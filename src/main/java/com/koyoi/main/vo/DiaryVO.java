cf9d6482ac7696478e95a4125e100b6e7360a212
nuri <dkssnfl05@naver.com>
2025-03-13 12:41:00 +0900

    feat: Add DiaryService and integrate calendar event API in controller

--- src/main/java/com/koyoi/main/vo/DiaryVO.java
+++ src/main/java/com/koyoi/main/vo/DiaryVO.java
@@ -7,10 +7,12 @@ import java.time.format.DateTimeFormatter;
 @Data
 public class DiaryVO {
     private int diary_id;
-    private String diary_title;
+    private String user_id;
+//    private String diary_title;
     private String diary_content;
     private LocalDateTime created_at;
 
+    private String emotion_emoji;
 
     public String getFormattedCreatedAt() {
         if (created_at != null) {
